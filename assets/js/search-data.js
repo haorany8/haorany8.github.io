4119b3a2ddade3dd6c62b0cfb0793d394cb1f1ef
haorany8 <75158631+haorany8@users.noreply.github.com>
2025-10-25 21:23:32 +0000

    Deploying to gh-pages from @ haorany8/haorany8.github.io@b1592fc62437781b2ac8e25a2556f614c01f050a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-paper-push-anything-is-up-on-arxiv-the-link-is-https-arxiv-org-pdf-2510-19974",
-          title: 'Our paper Push Anything is up on Arxiv! The link is https://arxiv.org/pdf/2510.19974',
+            },},{id: "news-our-paper-push-anything-is-up-on-arxiv-the-link-is-https-arxiv-org-pdf-2510-19974-and-the-project-website-is-https-dairlab-github-io-push-anything",
+          title: 'Our paper Push Anything is up on Arxiv! The link is https://arxiv.org/pdf/2510.19974, and...',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
